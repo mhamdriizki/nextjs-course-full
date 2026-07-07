02ac4bda94ccbaf8ebef6161dfb101965a9e3dd5
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2026-07-07 14:18:52 +0700

    feat: Applied Link navigation

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import Link from "next/link";
+import { usePathname } from "next/navigation"
+
+export default function Navbar() {
+  const pathName = usePathname();
+
+  return (
+    <nav style={{ background:"#1f2937", padding:"1rem", display:"flex", gap:"1rem" }}>
+      {/* Kita selalu pakai Link untuk pindah halaman */}
+      <Link
+        href="/"
+        style={{
+          color: pathName === "/" ? "#34d399" : "white",
+          fontWeight: pathName === "/" ? "bold" : "normal",
+          textDecoration: "none"
+        }}>
+          Home
+      </Link>
+
+      <Link
+        href="/blog"
+        style={{
+          color: pathName === "/blog" ? "#34d399" : "white",
+          fontWeight: pathName === "/blog" ? "bold" : "normal",
+          textDecoration: "none"
+        }}>
+          Blog
+      </Link>
+
+      <Link
+        href="/dashboard"
+        style={{
+          color: pathName === "/dashboard" ? "#34d399" : "white",
+          fontWeight: pathName === "/dashboard" ? "bold" : "normal",
+          textDecoration: "none"
+        }}>
+          Dashboard
+      </Link>
+      
+      <Link
+        href="/katalog"
+        style={{
+          color: pathName === "/katalog" ? "#34d399" : "white",
+          fontWeight: pathName === "/katalog" ? "bold" : "normal",
+          textDecoration: "none"
+        }}>
+          Katalog
+      </Link>
+
+      <Link
+        href="/member"
+        style={{
+          color: pathName === "/member" ? "#34d399" : "white",
+          fontWeight: pathName === "/member" ? "bold" : "normal",
+          textDecoration: "none"
+        }}>
+          Member
+      </Link>
+
+    </nav>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Navbar from "./components/Navbar"
 
 export default function RootLayout({
   children
@@ -8,14 +9,7 @@ export default function RootLayout({
   return (
     <html lang="id">
       <body style={{ margin:0, padding:0, fontFamily: 'Arial, sans-serif' }}>
-        <nav style={{ padding: '1rem', background:'#111', color: 'white', display: 'flex', gap: '1rem' }}>
-          <strong>Next Js Blog</strong>
-          <Link href="/" style={{color: 'white'}}>Home</Link>
-          <Link href="/blog" style={{color: 'white'}}>Blog</Link>
-          <Link href="/dashboard" style={{color: 'white'}}>Dashboard</Link>
-          <Link href="/katalog" style={{color: 'white'}}>Katalog</Link>
-          <Link href="/galeri" style={{color: 'white'}}>Galeri</Link>
-        </nav>
+        <Navbar/>
 
         <main style={{ padding: '2rem', minHeight: '80vh'}}>
           {children}
