38ab18dc00935cf87ab95f97babe04be37e89432
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2026-09-03 22:48:13 +0700

    fix: disable light theme and dark theme

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
-import { Moon, Sun } from "lucide-react";
+// import { Moon, Sun } from "lucide-react";
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
@@ -14,9 +14,9 @@ export function ThemeToggle() {
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       className='rounded-full bg-slate-800 text-white border-slate-700 hover:bg-slate-700'>
       
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
+      {/* <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
 
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/> */}
 
       <span className="sr-only">Toggle Theme</span>
     </Button>
